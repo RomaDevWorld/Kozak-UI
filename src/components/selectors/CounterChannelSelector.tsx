b51f7d3c3633d7dd508e501ccc30da8882cac95e
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-08-30 21:17:35 +0300

    add: counter channel & name selectors

---
+++ src/components/selectors/CounterChannelSelector.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Modules } from '@/types/Modules'
+import PartialChannel from '@/types/PartialChannel'
+import axios from 'axios'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+
+const CounterChannelSelector = ({ channels, modules }: { channels: PartialChannel[]; modules: Modules }) => {
+  const guildId = modules.guildId
+
+  const [selectedChannel, setSelectedChannel] = useState(modules.counter.channelId || '')
+  const [saving, setSaving] = useState(false)
+
+  const handleChannelSelect = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedChannel(event.target.value)
+
+    try {
+      setSaving(true)
+      await axios.post(`${process.env.NEXT_PUBLIC_APIURL}/guilds/${guildId}/admin/modules`, { 'counter.channelId': selectedChannel }, { withCredentials: true })
+
+      toast.success('Setting applied!')
+    } catch (err) {
+      toast.error('Request failed!')
+      console.error(err)
+    } finally {
+      setSaving(false)
+    }
+  }
+
+  return (
+    <div className="flex flex-col">
+      <select disabled={saving} id="counterChannelSelect" value={selectedChannel} onChange={handleChannelSelect}>
+        {channels
+          .filter((channel) => [2, 4].includes(channel.type))
+          .map((channel) => (
+            <option key={channel.id} value={channel.id}>{`${channel.type === 2 ? 'Category' : 'Voice'}: ${channel.name} (ID: ${channel.id})`}</option>
+          ))}
+      </select>
+    </div>
+  )
+}
+
+export default CounterChannelSelector
---
+++ src/components/text-inputs/CounterNameInput.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { Modules } from '@/types/Modules'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
+
+const CounterNameInput = ({ modules }: { modules: Modules }) => {
+  const guildId = modules.guildId
+
+  const [channelName, setChannelName] = useState(modules.counter?.label || '')
+  const [saving, setSaving] = useState(false)
+
+  const saveSelection = async () => {
+    try {
+      setSaving(true)
+      await axios.post(`${process.env.NEXT_PUBLIC_APIURL}/guilds/${guildId}/admin/modules`, { 'counter.label': channelName }, { withCredentials: true })
+
+      toast.success('Setting applied!')
+    } catch (err) {
+      toast.error('Request failed!')
+      console.error(err)
+    } finally {
+      setSaving(false)
+    }
+  }
+
+  return (
+    <div className="flex gap-2">
+      <input type="text" onChange={(e) => setChannelName(e.target.value)} value={channelName} placeholder="Members: ON/MEM" maxLength={50} />
+      <button onClick={saveSelection} disabled={saving || channelName === modules.counter?.label || channelName.length === 0}>
+        {saving ? 'Saving...' : 'Save'}
+      </button>
+    </div>
+  )
+}
+
+export default CounterNameInput
