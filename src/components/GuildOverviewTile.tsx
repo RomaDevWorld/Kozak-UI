4158d820f6f31cd1d1b5805e7e78d7a15cc603b1
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-09-01 22:07:25 +0300

    fix(ratelimit issue): disable prefetch in overview

--- src/components/GuildOverviewTile.tsx
+++ src/components/GuildOverviewTile.tsx
@@ -7,7 +7,7 @@ type TileProps = {
 
 const GuildOverviewTile = ({ label, href }: TileProps) => {
   return (
-    <Link href={href.toString()} className="bg-neutral-400 py-4 px-5 rounded-lg">
+    <Link prefetch={false} href={href.toString()} className="bg-neutral-400 py-4 px-5 rounded-lg">
       <div className="text-xl">{label}</div>
     </Link>
   )
