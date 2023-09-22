04bb97747d50ba855b88a1929491f68e3c486db7
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-09-22 23:05:40 +0300

    add: xp notifications toggle

--- src/app/dashboard/[guildId]/modules/leveling/page.tsx
+++ src/app/dashboard/[guildId]/modules/leveling/page.tsx
@@ -1,6 +1,7 @@
 import MinMaxXp from '@/components/selectors/numbers/MinMaxXp'
 import XpCooldownSelector from '@/components/selectors/numbers/XpCooldownSelector'
 import LevelingModuleToggle from '@/components/toggles/LevelingModuleToggle'
+import XpNotificationsToggle from '@/components/toggles/XpNotificationsToggle'
 import fetchGuildModules from '@/utils/api/fetchGuildModules'
 import React from 'react'
 
@@ -21,6 +22,13 @@ const LevelingModulePage = async ({ params }: { params: { guildId: string } }) =
       <div className="flex flex-row gap-10 items-center">
         <h1 className="text-xl font-bold">XP cooldown</h1>
         <XpCooldownSelector modules={modules} />
+      </div>
+      <div className="flex flex-col gap-2 items-center">
+        <h1 className="text-xl font-bold">Notifications</h1>
+        <div className="flex-row flex gap-4 items-center">
+          <h1>On lvl up:</h1>
+          <XpNotificationsToggle modules={modules} />
+        </div>
       </div>
     </div>
   )
