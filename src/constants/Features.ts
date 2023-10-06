d9016678eb47b30ef67db3d743fd50cb57991510
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-10-06 20:10:01 +0300

    update: extend homepage

---
+++ src/constants/Features.ts
@@ -0,0 +1,49 @@
+const features = [
+  {
+    title: 'Private Voice Channels',
+    description: "Automatically create private voice channels only for your friends you've invited. Make a party, combine a team in a meter of seconds.",
+    image: '/feature-1.png',
+  },
+  {
+    title: 'Audit Log',
+    description: 'Watch over everything that is happening. The bot monitors a large number of events on the server and reports them to a specialized channel.',
+    image: '/feature-2.png',
+  },
+  {
+    title: 'Watch The Numbers Grow',
+    description: "View the activity of server members using the online counter. Create a voice channel or category as an online counter so that the bot regularly updates its name to display current information about your server's activity.",
+    image: '/feature-3.png',
+  },
+  {
+    title: 'Automatically Restore Roles and Nickname',
+    description: "Accidentally left your favorite server?. Don't worry, the bot will remember your roles and nickname, and will give them to you as soon as you return to the party",
+    image: '/feature-4.png',
+  },
+  {
+    title: 'Keep Members In A Good Mood',
+    description: 'Bored? Entertain yourself by trying exciting game commands. Try 8ball, cat, dog, dice',
+    image: '/feature-5.png',
+  },
+  {
+    title: 'Voting System',
+    description: 'Have an important question to your server members? Create an anonymous poll.',
+    image: '/feature-6.png',
+  },
+  {
+    title: 'Leveling System',
+    description: 'Decide who is the most active member on the server using leveling system.',
+    image: '/feature-7.png',
+  },
+  {
+    title: '10X your server moderators',
+    description: 'Large amount of mod-related features: Reporting, Better mute command, Warning system and Purge.',
+    image: '/feature-8.png',
+  },
+  {
+    title: 'Keep the Russians Away',
+    description: 'Prevent members from reacting with forbidden flag. Automatically deletes and bans a member who reacts with a flag of a terrorist country.',
+    image: '/feature-9.png',
+  },
+]
+
+export default features
