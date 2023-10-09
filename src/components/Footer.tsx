782084a21feeab256c16b15d4e459dd4aa6b76ad
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-10-09 16:24:36 +0300

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,13 @@ const Footer = () => {
     <footer className="w-full mt-10 px-5 flex justify-between items-center py-5 border-t-2 border-t-white">
       <Link href={'/'} className="flex items-center gap-2">
         <Image src={'/avatar.webp'} alt="Kozak logo" width={40} height={40} className="rounded-md" />
-        <h1 className="font-bold">kozakbot.xyz</h1>
+        <h1 className="font-bold hidden sm:block">kozakbot.xyz</h1>
       </Link>
       <div className="flex gap-2">
-        <Link href={process.env.LINKS_GITHUB as string}>
+        <Link href={process.env.LINKS_GITHUB || 'https://github.com'}>
           <Image className="invert" src="/github.svg" alt="github social" height={25} width={25} />
         </Link>
-        <Link href={process.env.LINKS_DISCORD as string}>
+        <Link href={process.env.LINKS_DISCORD || 'https://discord.com'}>
           <Image className="invert" src="/discord.svg" alt="discord social" height={25} width={25} />
         </Link>
       </div>
